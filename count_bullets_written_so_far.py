dc6922109954800fba3a4c416b85ff48d87aae63
lavedon <lavedon@gmail.com>
2017-09-20 07:27:17 -0400

    The counting program does not yet work.  will need to use RegEx because so many bullets have natural () in them.

--- count_bullets_written_so_far.py
+++ count_bullets_written_so_far.py
@@ -1,4 +1,5 @@
 # Count bullets written
+# THIS DOES NOT YET WORK
 from tkinter import *
 from tkinter import filedialog
 root = Tk()
@@ -8,9 +9,21 @@ fileName = filedialog.askopenfilename( filetypes = (("TEXT files", "*.txt"),
 with open(fileName, 'r') as f:
     lines = f.readlines()
 
+# Print out what lines were read:
+print("Printing out each line")
+for i in lines:
+    print(i)
 
 # remove lines with ()
 for i in lines:
-    if '(' in lines:
-        del i
-print("You have: " + len(lines) + "written so far")
+    if '(' in i:
+        print("found this: " + i)
+        quit()
+    if i == '':
+        lines.pop(i)
+print("Now I removed the knowledge bombs...\n")
+print("Printing out what is left")
+for i in lines:
+    print(i)
+
+print("You have: " + str(len(lines)) + "written so far")
